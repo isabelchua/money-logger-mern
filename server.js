6e39acd001683a6048c8961dfd14b5adb0a55fad
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-08 12:45:56 -0700

    connected to db

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
 
+connectDB();
+
+const transactions = require("./routes/transactions");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+app.use("/api/v1/transactions", transactions);
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,3 +23,4 @@ app.listen(
 			.bold
 	)
 );
+// app.get("/", (req, res) => res.send("Hello"));
